954f6385bf6a10b36605820fb04c262fed5f02fb
ntruong <nt31415@gmail.com>
2018-09-05 09:21:48 -0500

    no more ugly notebooks

--- site.hs
+++ site.hs
@@ -14,91 +14,76 @@ import           Text.Pandoc.Options
 
 main :: IO ()
 main = hakyll $ do
-  match "css/**" $ do
-    route   idRoute
-    compile compressCssCompiler
-
-  match "js/**" $ do
-    route   idRoute
-    compile copyFileCompiler
-
-  match "static/**" $ do
-    route   idRoute
-    compile copyFileCompiler
-
-  match "CNAME" $ do
-    route   idRoute
-    compile copyFileCompiler
 
   match "index.html" $ do
     route idRoute
     compile $ getResourceBody
       >>= relativizeUrls
 
-  create ["notes/index.html"] $ do
-    route idRoute
-    compile $ do
-      let ctx = contexts [ cssCtx ["css/article.css"]
-                         , indexCtx $ loadAll $ indexPattern "notes/*"
-                         , constField "title" "Notes" ]
+  match "CNAME" $ do
+    route   idRoute
+    compile copyFileCompiler
 
-      makeItem ""
-        >>= loadAndApplyTemplate "templates/book.html" ctx
-        >>= loadAndApplyTemplate "templates/default.html" ctx
-        >>= relativizeUrls
+  match "js/**" $ do
+    route   idRoute
+    compile copyFileCompiler
 
-  notebook "notes/algebra" "Algebraic Structures"
+  match "css/**" $ do
+    route   idRoute
+    compile compressCssCompiler
 
-  match "templates/*" $ compile templateBodyCompiler
+  match "static/**" $ do
+    route   idRoute
+    compile copyFileCompiler
 
---------------------------------------------------------------------------------
-
-notebook :: String -> String -> Rules ()
-notebook id title = do
-  create [fromFilePath (id ++ "/index.html")] $ do
-    route $ setExtension "html"
-    compile $ do
-      notes <- loadSorted $ pagesPattern id
-      let ctx = contexts [ cssCtx ["css/article.css"]
-                         , indexCtx notes
-                         , constField "title" title ]
-
-      makeItem ""
-        >>= loadAndApplyTemplate "templates/book.html" ctx
-        >>= loadAndApplyTemplate "templates/default.html" ctx
-        >>= relativizeUrls
-
-  tags <- buildTags (pagesPattern id) $ fromCapture "tags/*.html"
+  tags <- buildTags (fromGlob "notes/**") (fromCapture "tags/*.html")
 
   tagsRules tags $ \tag pattern -> do
     route idRoute
     compile $ do
       let ctx = contexts [ indexCtx (loadAll pattern)
-                         , constField "title" ("[" ++ tag ++ "]") ]
+                         , constField "title" (":" ++ tag) ]
 
       makeItem ""
         >>= loadAndApplyTemplate "templates/tag.html" ctx
         >>= loadAndApplyTemplate "templates/default.html" ctx
         >>= relativizeUrls
 
-  match (pagesPattern id) $ do
+  match "notes/**" $ do
     route $ setExtension "html"
     compile $ do
       let ctx = contexts [ cssCtx ["css/article.css"]
-                         , noteCtx tags ]
+                         , noteCtx tags
+                         ]
 
       pandocMathCompiler
         >>= loadAndApplyTemplate "templates/article.html" ctx
         >>= loadAndApplyTemplate "templates/default.html" ctx
         >>= relativizeUrls
 
+  create ["notes/index.html"] $ do
+    route idRoute
+    compile $ do
+      let notesPattern = (fromGlob "notes/**") .&&. complement "notes/index.html"
+          ctx = contexts [ cssCtx ["css/article.css"]
+                         , (indexCtx . loadAll) notesPattern
+                         , constField "title" "Notes"
+                         ]
+
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/book.html" ctx
+        >>= loadAndApplyTemplate "templates/default.html" ctx
+        >>= relativizeUrls
+
+  match "templates/*" $ compile templateBodyCompiler
+
 --------------------------------------------------------------------------------
 
 indexCtx :: Compiler [Item String] -> Context String
-indexCtx pages = listField "pages" defaultContext pages
+indexCtx = listField "pages" defaultContext
 
 noteCtx :: Tags -> Context String
-noteCtx tags = tagsField "tags" tags
+noteCtx = tagsField "tags"
 
 cssCtx :: [Identifier] -> Context String
 cssCtx styles = listField "css" defaultContext (loadAll $ fromList styles)
@@ -107,25 +92,6 @@ contexts :: [Context String] -> Context String
 contexts ctxs = mconcat ctxs <> defaultContext
 
 --------------------------------------------------------------------------------
-
-indexPattern :: String -> Pattern
-indexPattern id = fromGlob $ id ++ "/index.html"
-
-pagesPattern :: String -> Pattern
-pagesPattern id = (fromGlob $ id ++ "/*") .&&. complement (indexPattern id)
-
-loadSorted :: (Binary a, Typeable a, MonadMetadata m) =>
-              Pattern -> Compiler (m [Item a])
-loadSorted pattern = fmap (sortMd "ordering") $ loadAll pattern
-
--- | Sorts items by a specified metadata field (with ordering)
-sortMd :: MonadMetadata m => String -> [Item a] -> m [Item a]
-sortMd field =
-  sortByM $ \x -> getMetadataField (itemIdentifier x) field
-  where
-    sortByM :: (Monad m, Ord k) => (a -> m k) -> [a] -> m [a]
-    sortByM f xs = liftM (map fst . sortBy (comparing snd)) $
-                   mapM (\x -> liftM (x,) (f x)) xs
 
 -- | Compile template with LaTeX
 pandocMathCompiler =
